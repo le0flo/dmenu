8be330737c50040caed01215f8ff98bcd9356ca6
Leonardo <leonflorenzano@gmail.com>
2024-06-16 17:16:03 +0200

    Riorganizzato il file di configurazione

--- config.def.h
+++ config.def.h
@@ -1,30 +1,27 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 300;                    /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 0;               /* add an defined amount of pixels to the bar height */
+/* appearance */
+static int topbar = 1;
+static int centered = 0;
+static int min_width = 300;
+static const int user_bh = 0;
 
+/* font */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"BigBlueTermPlus Nerd Font:size=8"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* prompt */
+static const char *prompt = NULL;
+
+/* colors */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#97bb72" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+/* layout */
+static unsigned int lines = 0;
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
 static unsigned int border_width = 2;
